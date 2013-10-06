c27a3da4c96edef21297f99cc8d4a214bf47d375
Jonathan Hall <Jonathan.Hall@webappuk.com>
2013-10-06 18:40:25 +0100

    Add: app/ts directory. Update: Old application source files to TypeScript.

---
+++ app/ts/controllers.ts
@@ -0,0 +1,12 @@
+/// <reference path="../lib/typings/angular/angular.d.ts" />
+'use strict';
+
+/* Controllers */
+
+angular.module('myApp.controllers', []).
+  controller('MyCtrl1', [function() {
+
+  }])
+  .controller('MyCtrl2', [function() {
+
+  }]);
---
+++ app/ts/filters.ts
@@ -0,0 +1,11 @@
+/// <reference path="../lib/typings/angular/angular.d.ts" />
+'use strict';
+
+/* Filters */
+
+angular.module('myApp.filters', []).
+  filter('interpolate', ['version', function(version) {
+    return function(text) {
+      return String(text).replace(/\%VERSION\%/mg, version);
+    }
+  }]);
